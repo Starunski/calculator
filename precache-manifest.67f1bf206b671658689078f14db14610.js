1f1ade0e95c3c61701920b98ae09ba10fbce3565
Dmitri Starunski <dmitriy.starunskiy@gmail.com>
2020-06-29 15:09:37 +0200

    Updates

---
+++ precache-manifest.67f1bf206b671658689078f14db14610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c69b05f5480a9cbc861559d1a9f8f90",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "02419214ab15511eca96",
+    "url": "/calculator/static/css/main.6d3615f9.chunk.css"
+  },
+  {
+    "revision": "319d8c5010f731819932",
+    "url": "/calculator/static/js/2.62465842.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.62465842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02419214ab15511eca96",
+    "url": "/calculator/static/js/main.556590c5.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
